1883c2800d38f546a743e69cc041089bbcc50e62
Luiggi Marquez <nf_snake@hotmail.com>
2022-09-07 10:14:25 -0300

    Desafio 4 API RESTful

---
+++ desafio_4.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080;
+const productos = require('./routes/productos.js');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/api/productos', productos)
+app.use(express.static(__dirname+'/public'));
+
+/* 
+app.get('/api/productos',((req,res)=>{
+    
+  
+    res.send("respuesta, esto es una respuesta")
+  
+
+})) */
+
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`))
