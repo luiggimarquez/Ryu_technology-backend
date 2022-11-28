9b4ff69ac0cdc0072d285fc571e613f3e4b6ad49
luiggimarquez <95132450+luiggimarquez@users.noreply.github.com>
2022-11-28 01:56:45 -0300

    Modificacion calculo numeros aleatorios

--- routes/routerRandom.js
+++ routes/routerRandom.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { fork } from 'child_process'
-import { on } from "events";
+
 
 const routerRandom = Router()
 
@@ -8,19 +8,18 @@ const routerRandom = Router()
 routerRandom.get('/',(req,res)=>{
     
     const cant  = req.query
-  
     const childProcess = fork('./utils/randomNumbers.js')
-    console.log("aqui: ", cant)
     
     childProcess.send(cant)
     childProcess.on('message', result =>{
+    
+        //result = JSON.stringify(result)
         
-        console.log("rsultado random: ", result)
+        //res.render('randomNumbers',{result})
+        res.json(result)
     }) 
     
-    res.json({error: 404})
-
-
 })
+
 
 export default routerRandom
--- utils/randomNumbers.js
+++ utils/randomNumbers.js
@@ -1,30 +1,33 @@
 
 function randomNumbers(numbersRamdon) {
     
-    let resultRandom = 0
-    let numbers= parseInt(numbersRamdon) || 1000000
+    let resultRandom = []
+    let numbers= numbersRamdon || 100000000
+
+    console.log('calculando : ',numbers)
 
     for (let i=0; i<numbers; i++){
 
         resultRandom.push((Math.floor(Math.random()*(1000))+1))
     }
 
-    let resultCount = 0
-
+    let resultCount = {}
+    
     resultRandom.forEach( count =>{
+        
         resultCount[count] = resultCount[count] + 1 || 1
-
     })
-
+    
     return  resultCount
 }
 
-let result = 0
+
 
 process.on('message', (numbers) => {
 
     console.log("Inicio Subproceso: ");
-    result = randomNumbers(parseInt(numbers.cant))
+    let result = randomNumbers(parseInt(numbers.cant))
+    console.log('listo a enviar: ',result)
     process.send(result);
     process.exit()
     
